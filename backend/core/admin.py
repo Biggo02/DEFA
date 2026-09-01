18900af0612a8e125856f5b544e945d09ad90b13
Biggo02 <o19549991@gmail.com>
2026-09-01 06:12:04 +0200

    chore: register DEFA operational models in admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-for model in [Profile, Address, Employment, Business, Reference, LoanApplication, VerificationVisit, Loan, Installment, Payment, PaymentReceipt, Consent, AuditLog]:
-    admin.site.register(model)
+MODELS = [
+    Profile, Address, Employment, Business, Reference, UploadedDocument,
+    LoanApplication, VerificationVisit, AgentAssignment, LocationConsent,
+    LocationRecord, Loan, Installment, Contract, Payment, PaymentReceipt,
+    CollectionVisit, Notification, FraudAlert, SystemSetting, Consent, AuditLog,
+]
+for model in MODELS:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
