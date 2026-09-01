6b0c6a2e483efdd5d8656265b1a8a1cfeef33d4d
Biggo02 <o19549991@gmail.com>
2026-09-01 06:53:46 +0200

    feat: connect admin risk and agent field screens

--- src/routes/defaScreens.js
+++ src/routes/defaScreens.js
@@ -8,6 +8,9 @@ import PaymentHistoryPage from '../pages/PaymentHistoryPage';
 import ReceiptListPage from '../pages/ReceiptListPage';
 import AgentScannedLoanPage from '../pages/AgentScannedLoanPage';
 import AdminApplicationReviewPage from '../pages/AdminApplicationReviewPage';
+import AdminOperationsPage from '../pages/AdminOperationsPage';
+import AgentFieldPage from '../pages/AgentFieldPage';
+import AdminRiskPage from '../pages/AdminRiskPage';
 
 export const DEFA_SCREENS = Object.freeze({
   'loan-application': LoanJourneyPage,
@@ -19,4 +22,9 @@ export const DEFA_SCREENS = Object.freeze({
   'receipts': ReceiptListPage,
   'scanned-loan': AgentScannedLoanPage,
   'application-review': AdminApplicationReviewPage,
+  'applications': AdminOperationsPage,
+  'application-review-dashboard': AdminRiskPage,
+  'credit-score': AdminRiskPage,
+  'field-visits': AgentFieldPage,
+  'collections': AgentFieldPage,
 });
