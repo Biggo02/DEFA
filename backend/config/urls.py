c34f00e1f93aec2fb8635709689cc3bb6b449945
Biggo02 <o19549991@gmail.com>
2026-09-02 11:59:52 +0200

    feat: add DEFA 404 handler

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
     path('', include('core.web_urls')),
 ]
+
+handler404 = 'config.urls.error_404'
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+def error_404(request, exception):
+    return render(request, '404.html', status=404)
