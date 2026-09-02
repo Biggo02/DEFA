a74e39912cd9b2b84d23656319505ad044b03766
Biggo02 <o19549991@gmail.com>
2026-09-02 08:58:48 +0200

    feat: use Django as DEFA frontend

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,6 +3,10 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path('admin/', admin.site.urls), path('api/', include('core.urls'))]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),
+    path('', include('core.web_urls')),
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
